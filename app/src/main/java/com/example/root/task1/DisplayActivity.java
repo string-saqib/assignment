e0308a644302fe4a67b295f51aaa18b9905aec77
string-saqib <s.sarwar@string.org.in>
2017-08-06 18:30:40 +0530

    remote url changed

--- app/src/main/java/com/example/root/task1/DisplayActivity.java
+++ app/src/main/java/com/example/root/task1/DisplayActivity.java
@@ -2,42 +2,40 @@ package com.example.root.task1;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+
 
 public class DisplayActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
-    private Button edit_button;
+    private Button editButton;
 
-    private static Bundle back_bundle;
+    private static Bundle infoBundle;
 
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-        setContentView(R.layout.fragment_details);
+        setContentView(R.layout.activity_display);
 
         DisplayFragment details = new DisplayFragment();
         details.setArguments(this.getIntent().getExtras());
+        getSupportFragmentManager().beginTransaction().commit();
 
-        back_bundle = bundle;
-        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("DisplayScreen");
 
-        edit_button = (Button) findViewById(R.id.edit_button);
-
-        edit_button.setOnClickListener(new View.OnClickListener() {
+        infoBundle = bundle;
+        editButton = (Button) findViewById(R.id.edit_button);
+        editButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent back_intent = new Intent(DisplayActivity.this, LoginActivity.class);
-                back_intent.putExtras(back_bundle);
-                startActivity(back_intent);
+                Intent backIntent = new Intent(DisplayActivity.this, LoginActivity.class);
+                backIntent.putExtras(infoBundle);
+                startActivity(backIntent);
                 finish();
             }
 
--- app/src/main/java/com/example/root/task1/DisplayFragment.java
+++ app/src/main/java/com/example/root/task1/DisplayFragment.java
@@ -17,20 +17,7 @@ public class DisplayFragment extends Fragment {
 
     private static Bundle backBundle;
     private TextView mTextView;
-
-
-    private OnFragmentInteractionListener mListener;
-
     public DisplayFragment() {
-        // Required empty public constructor
-    }
-
-    public static DisplayFragment newInstance(String param1, String param2) {
-        DisplayFragment fragment = new DisplayFragment();
-        Bundle args = new Bundle();
-        //args.putString(ARG_PARAM1, param1);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
@@ -51,7 +38,7 @@ public class DisplayFragment extends Fragment {
         String gender = backBundle.getString("Gender");
         String city = backBundle.getString("City");
 
-        mTextView = (TextView) inflatedView.findViewById(R.id.user_text);
+        mTextView = inflatedView.findViewById(R.id.user_text);
         mTextView.setText(username);
         mTextView.append("\n");
         mTextView.append(emailID + "\n");
@@ -59,43 +46,5 @@ public class DisplayFragment extends Fragment {
         if (!city.equals("---Select---"))
             mTextView.append("\n" + city);
         return inflatedView;
-    }
-
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
